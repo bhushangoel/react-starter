a0da08adb02160e3f43fb9f0b879208cf2dd3eed
bhushangoel <bgoel1991@gmail.com>
2017-03-22 00:42:07 +0530

    creating new components

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,26 +1,16 @@
-import React, {Component} from 'react';
-import videoListItem from './video_list_item';
+import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
     const videoItems = props.videos.map((video) => {
-        return <videoListItem key={video.etag} video={video} />
+        return <VideoListItem onVideoSelect={props.onVideoSelect} key={video.etag} video={video}/>
     });
+
     return (
-        <ul className="col-md-4 list-group">
+        <ul className="col-md-8 list-group">
             {videoItems}
         </ul>
     )
 };
-
-// class VideoList extends Component {
-//
-//     render() {
-//         return (
-//             <ul className="col-md-4 list-group">
-//                 <videoListItem />
-//             </ul>
-//         )
-//     }
-// }
 
 export default VideoList;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import YTsearch from 'youtube-api-search';
 //components
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = "AIzaSyB5zTHnYgbIqY3nbZkQK2IMdXyfsmO6TdI";
 
@@ -18,11 +19,14 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {videos: []};
+        this.state = {videos: [], selectedVideo: null};
 
         YTsearch({key: API_KEY, term: 'bmw'}, (videos) => {
             //when key and value have the same name, then we can condense them to one variable only
-            this.setState({videos});
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
@@ -31,7 +35,8 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoList videos={this.state.videos}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos}/>
             </div>
         )
     }
